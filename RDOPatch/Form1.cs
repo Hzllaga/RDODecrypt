707ce3b18159ffc4024754b83f3461323f2abe2f
Hzllaga <40329078+Hzllaga@users.noreply.github.com>
2020-05-15 13:18:29 +0800

    优化程序

--- RDOPatch/Form1.cs
+++ RDOPatch/Form1.cs
@@ -1,4 +1,4 @@
-﻿using RDO.Data;
+using RDO.Data;
 using RDO.Data.Types;
 using System;
 using System.IO;
@@ -18,46 +18,31 @@ namespace RDO
 
         private void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = "";
-            Root root = (Root)smethod_1("Connections.rdo", Type.GetType("RDO.Data.Types.Root"));
-
-            //IdCards idcard = (IdCards)root.Nodes[0];
-            Connections conn = (Connections)root.Nodes[1];
-            for (int i = 0; i < conn.Nodes.Count; i++)
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "RDO Files (*.rdo)|*.rdo|DAT Files (*.dat)|*.dat";
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                try
+                Root root = (Root)loadData(openFileDialog.FileName, Type.GetType("RDO.Data.Types.Root"));
+                Connections conn = (Connections)root.Nodes[1];
+                for (int i = 0; i < conn.Nodes.Count; i++)
                 {
-                    Folder folder = (Folder)conn.Nodes[i];
-                    for (int j = 0; j < folder.Nodes.Count; j++)
+
+                    if (conn.Nodes[i].GetType().ToString() == "RDO.Data.Types.Folder")
                     {
-                        try
-                        {
-                            RemoteDesktop rdp = (RemoteDesktop)folder.Nodes[j];
-                            richTextBox1.Text += " Host: " + rdp.Host + " Username: " + rdp.User + " Password: " + smethod_5(rdp.Password) + " Description: " + rdp.Description + "\n";
-                        }
-                        catch (Exception)  //二层文件夹，懒得判断，有需求的自己改写。
-                        {
-                            Folder folder2 = (Folder)folder.Nodes[j];
-                            for (int k = 0; k < folder2.Nodes.Count; k++)
-                            {
-                                RemoteDesktop rdp2 = (RemoteDesktop)folder2.Nodes[k];
-                                richTextBox1.Text += " Host: " + rdp2.Host + " Username: " + rdp2.User + " Password: " + smethod_5(rdp2.Password) + " Description: " + rdp2.Description + "\n";
-                            }
-                        }
-
+                        ParseFolder((Folder)conn.Nodes[i]);
                     }
-
+                    else if (conn.Nodes[i].GetType().ToString() == "RDO.Data.Types.RemoteDesktop")
+                    {
+                        ParseRemoteDesktop((RemoteDesktop)conn.Nodes[i]);
+                    }
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
+                label1.Text = "记录数： " + (dataGridView1.Rows.Count - 1).ToString();
             }
-
-            string[] count = richTextBox1.Lines;
-            label1.Text = "记录数： " + (count.Length - 1).ToString();
         }
-        public static object smethod_1(string string_0, Type type_0)
+        public static object loadData(string string_0, Type type_0)
         {
             Stream stream = null;
             object result = null;
@@ -69,45 +54,55 @@ namespace RDO
             return result;
         }
 
-        public static byte[] smethod_4(byte[] byte_0, byte[] byte_1, byte[] byte_2)
+        public static string Decrypt(string string_0)
         {
-            MemoryStream memoryStream = new MemoryStream();
-            Rijndael rijndael = Rijndael.Create();
-            rijndael.Key = byte_1;
-            rijndael.IV = byte_2;
-            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateDecryptor(), CryptoStreamMode.Write);
-            cryptoStream.Write(byte_0, 0, byte_0.Length);
-            cryptoStream.Close();
-            return memoryStream.ToArray();
+            try
+            {
+                string strPassword = "swevA2t62We?5Cr+he4Tac?_E!redafa?re5+2huv*$rU9eS8Ub4?W!!R+s7uthU";
+                byte[] array = new byte[string_0.Length / 2];
+                for (int i = 0; i < string_0.Length / 2; i++)
+                {
+                    int num = Convert.ToInt32(string_0.Substring(i * 2, 2), 16);
+                    array[i] = (byte)num;
+                }
+                PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(strPassword, new byte[]
+                {
+                    73, 118, 97, 110, 32, 77, 101, 100, 118, 101, 100, 101, 118
+                });
+                MemoryStream memoryStream = new MemoryStream();
+                Rijndael rijndael = Rijndael.Create();
+                rijndael.Key = passwordDeriveBytes.GetBytes(32);
+                rijndael.IV = passwordDeriveBytes.GetBytes(16);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateDecryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(array, 0, array.Length);
+                cryptoStream.Close();
+                return Encoding.Unicode.GetString(memoryStream.ToArray());
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+
         }
 
-        public static string smethod_5(string string_0)
+        public void ParseFolder(Folder folder)
         {
-            string strPassword = "swevA2t62We?5Cr+he4Tac?_E!redafa?re5+2huv*$rU9eS8Ub4?W!!R+s7uthU";
-            byte[] array = new byte[string_0.Length / 2];
-            for (int i = 0; i < string_0.Length / 2; i++)
+            for (int i = 0; i < folder.Nodes.Count; i++)
             {
-                int num = Convert.ToInt32(string_0.Substring(i * 2, 2), 16);
-                array[i] = (byte)num;
+                if (folder.Nodes[i].GetType().ToString() == "RDO.Data.Types.Folder")
+                {
+                    ParseFolder((Folder)folder.Nodes[i]);
+                }
+                else if (folder.Nodes[i].GetType().ToString() == "RDO.Data.Types.RemoteDesktop")
+                {
+                    ParseRemoteDesktop((RemoteDesktop)folder.Nodes[i]);
+                }
             }
-            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(strPassword, new byte[]
-            {
-                73,
-                118,
-                97,
-                110,
-                32,
-                77,
-                101,
-                100,
-                118,
-                101,
-                100,
-                101,
-                118
-            });
-            byte[] bytes = smethod_4(array, passwordDeriveBytes.GetBytes(32), passwordDeriveBytes.GetBytes(16));
-            return Encoding.Unicode.GetString(bytes);
+        }
+
+        public void ParseRemoteDesktop(RemoteDesktop rdp)
+        {
+            dataGridView1.Rows.Add(rdp.Host, rdp.User, Decrypt(rdp.Password), rdp.Description);
         }
     }
 }
